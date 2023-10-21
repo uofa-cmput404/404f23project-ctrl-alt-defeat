ef307249b378348c31ed0e1c1ce806d91b4017e2
Daniel Guo <guodaniel1@gmail.com>
2023-10-20 22:39:52 -0600

    Register for Requestors Working

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
+from flask_cors import CORS
+from app.requestors import bp as requestors_bp
 
 def create_app():
     app = Flask(__name__)
-
+    CORS(app)
     # Initialize Flask extensions here
 
     # Register blueprints here
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
+
+    from app.requestors import bp as requestors_bp
+    app.register_blueprint(requestors_bp, url_prefix='/requestors')   
 
     from app.posts import bp as posts_bp
     app.register_blueprint(posts_bp, url_prefix='/posts')    
--- server/app/schema.sql
+++ server/app/schema.sql
@@ -3,14 +3,15 @@ DROP TABLE IF EXISTS authors;
 DROP TABLE IF EXISTS requestors;
 DROP TABLE IF EXISTS admins;
 DROP TABLE IF EXISTS image_post;
-DROP TABLE IF EXISTS friendship;
+DROP TABLE IF EXISTS friends;
+DROP TABLE IF EXISTS friend_requests;
 DROP TABLE IF EXISTS likes;
 DROP TABLE IF EXISTS comments;
 DROP TABLE IF EXISTS shares;
 
 CREATE TABLE posts (
     post_id TEXT PRIMARY KEY,
-    author_id TEXT NOT NULL,
+    author_id INTEGER NOT NULL,
     date_posted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     title TEXT NOT NULL,
     content_type TEXT NOT NULL DEFAULT "text/plain",
@@ -22,13 +23,13 @@ CREATE TABLE posts (
 
 
 CREATE TABLE authors (
-    author_id TEXT PRIMARY KEY,
+    author_id INTEGER PRIMARY KEY,
     username TEXT NOT NULL,
     passwd TEXT NOT NULL
 );
 
 CREATE TABLE requestors (
-    requestor_id TEXT PRIMARY KEY,
+    requestor_id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT NOT NULL,
     passwd TEXT NOT NULL
 );
@@ -41,7 +42,7 @@ CREATE TABLE admins (
 
 CREATE TABLE image_post (
     img_id TEXT PRIMARY KEY,
-    author_id TEXT NOT NULL,
+    author_id INTEGER NOT NULL,
     img_url TEXT NOT NULL,
     visibility TEXT NOT NULL,
     date_posted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
@@ -50,33 +51,33 @@ CREATE TABLE image_post (
 
 -- if both AB, BA in friends, AB are true friends
 CREATE TABLE friends (
-    author_followee TEXT NOT NULL,
-    author_following TEXT NOT NULL,
+    author_followee INTEGER NOT NULL,
+    author_following INTEGER NOT NULL,
     FOREIGN KEY (author_followee) REFERENCES authors(author_id),
     FOREIGN KEY (author_following) REFERENCES authors(author_id)
 );
 
 -- if accepted, delete from friend_requests, add to friends
 CREATE TABLE friend_requests (
-    author_send TEXT NOT NULL,
-    author_receive TEXT NOT NULL,
-    FOREIGN KEY (author_send) REFERENCES authors(author_id)
-    FOREIGN KEY (author_receive) REFERENCES authors(author_id),
+    author_send INTEGER NOT NULL,
+    author_receive INTEGER NOT NULL,
+    FOREIGN KEY (author_send) REFERENCES authors(author_id),
+    FOREIGN KEY (author_receive) REFERENCES authors(author_id)
 );
 
 
 CREATE TABLE likes (
     like_id TEXT PRIMARY KEY,
-    like_author_id TEXT NOT NULL,
+    like_author_id INTEGER NOT NULL,
     post_id TEXT NOT NULL,
     time_liked TIMESTAMP NOT NULL,
     FOREIGN KEY (like_author_id) REFERENCES authors(author_id),
-    FOREIGN KEY (post_id) REFERENCES post(post_id)
+    FOREIGN KEY (post_id) REFERENCES posts(post_id)
 );
 
 CREATE TABLE comments (
     post_id TEXT PRIMARY KEY,
-    comment_author_id TEXT NOT NULL,
+    comment_author_id INTEGER NOT NULL,
     date_commented TIMESTAMP NOT NULL,
     FOREIGN KEY (comment_author_id) REFERENCES authors(author_id),
     FOREIGN KEY (post_id) REFERENCES post(post_id)
@@ -84,7 +85,7 @@ CREATE TABLE comments (
 
 CREATE TABLE shares(
     post_id TEXT PRIMARY KEY,
-    share_author_id TEXT NOT NULL,
+    share_author_id INTEGER NOT NULL,
     date_posted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (share_author_id) REFERENCES authors(author_id),
     FOREIGN KEY (post_id) REFERENCES post(post_id)
