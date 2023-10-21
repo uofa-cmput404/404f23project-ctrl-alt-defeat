5e3404c49da030275ff6749bf9a9ddaae6c6939a
Andrew Rosario <71676291+andrew-rosario@users.noreply.github.com>
2023-10-21 16:17:47 -0600

    Add prelim. Markdown display support to PostItem.js

--- client/social-distribution/src/components/PostItem.js
+++ client/social-distribution/src/components/PostItem.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import Markdown from "react-markdown";
 
+
+function get_content_as_elements(content_type, content){
+    if (content_type === "text/plain"){
+        return(<p>{content}</p>)
+    }
+    else if (content_type === "text/markdown"){
+        return(<Markdown>{content}</Markdown>)
+    }
+}
 function PostItem(props) {
   return (
     <li key={props.key}>
         <h3>{props.item.title}</h3>
         <div>Posted by: {props.item.author_id}</div>
         <div>{props.item.date_posted}</div>
-        <div>{props.item.content}</div>
+        <div>{get_content_as_elements(props.item.content_type,props.item.content)}</div>
     </li>
   )
 }
