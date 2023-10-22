514398340665f836cad26718169080943549d4e1
Mark Maligalig <markmaligalig@gmail.com>
2023-10-22 12:01:46 -0600

    Update view_posts to be consistent with schema

--- server/app/posts/routes.py
+++ server/app/posts/routes.py
@@ -12,14 +12,14 @@ def index():
         # Retrieve data from the request's JSON body
         print("data")
         request_data = request.get_json()
-        username = request_data['username']
+        author_id = request_data['author_id']
                 
         conn = get_db_connection()
         
         # Get all the posts from people who I'm following + posts who are public
         query = f"SELECT * FROM posts " \
                 f"WHERE posts.author_id in " \
-                f"(SELECT author_following FROM friends WHERE author_followee= '{username}') " \
+                f"(SELECT author_following FROM friends WHERE author_followee= '{author_id}') " \
                 f"OR visibility = 'public' " \
                 f"ORDER by date_posted DESC"
         
