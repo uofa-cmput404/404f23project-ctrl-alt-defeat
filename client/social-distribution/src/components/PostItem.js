840c288b7a06db2b239384ec44c7957a167c36b1
Andrew Rosario <71676291+andrew-rosario@users.noreply.github.com>
2023-10-21 18:01:03 -0600

    Add base64 PNG/JPEG image post view support

--- client/social-distribution/src/components/PostItem.js
+++ client/social-distribution/src/components/PostItem.js
@@ -4,10 +4,17 @@ import Markdown from "react-markdown";
 
 function get_content_as_elements(content_type, content){
     if (content_type === "text/plain"){
-        return(<p>{content}</p>)
+        return(<p>{content}</p>);
     }
     else if (content_type === "text/markdown"){
-        return(<Markdown>{content}</Markdown>)
+        return(<Markdown>{content}</Markdown>);
+    }
+    else if (content_type === "image/png;base64" || content_type === "image/jpeg;base64")
+    {
+        //var image = new Image();
+        //let decodedString = atob(content);
+        let tag = 'data:' + content_type + "," + content;
+        return (<img src={tag}/>)
     }
 }
 function PostItem(props) {
