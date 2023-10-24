a3ea617460e6ce1a7fba71b3f61afa605991e364
Andrew Rosario <71676291+andrew-rosario@users.noreply.github.com>
2023-10-24 16:24:30 -0600

    Add Markdown, image (base64/URL) support to PostItem.js

--- client/social-distribution/src/components/PostItem.js
+++ client/social-distribution/src/components/PostItem.js
@@ -1,4 +1,26 @@
 import React from 'react'
+import Markdown from "react-markdown";
+
+
+function get_content_as_elements(content_type, content){
+    if (content_type === "text/plain"){
+        return(<p>{content}</p>);
+    }
+    else if (content_type === "text/markdown"){
+        return(<Markdown>{content}</Markdown>);
+    }
+    else if (content_type === "image/png;base64" || content_type === "image/jpeg;base64")
+    {
+        //var image = new Image();
+        //let decodedString = atob(content);
+        let tag = 'data:' + content_type + "," + content;
+        return (<img src={tag}/>)
+    }
+    else if (content_type === "image/png;url" || content_type === "image/jpeg;url"){
+        //change width property or remove it, resizing will be done at the style level.
+        return(<img src={content} width="100px"/>);
+    }
+}
 
 function PostItem(props) {
   return (
@@ -6,7 +28,7 @@ function PostItem(props) {
         <h3>{props.item.title}</h3>
         <div>Posted by: {props.item.username}</div>
         <div>{props.item.date_posted}</div>
-        <div>{props.item.content}</div>
+        <div>{get_content_as_elements(props.item.content_type,props.item.content)}</div>
     </li>
   )
 }
