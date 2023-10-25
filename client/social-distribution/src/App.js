a73ff23eaa3b903692082920905a492f0d0ffbd2
philiponions <hueycoy9@gmail.com>
2023-10-24 18:54:47 -0600

    Resolve merge conflicts

--- client/social-distribution/src/App.js
+++ client/social-distribution/src/App.js
@@ -8,29 +8,36 @@ import { Navigate } from 'react-router-dom';
 
 export const UserContext = createContext();
 function App() {
+
   // Have the username and authorId state here and pass it down to pages and components
   const [username, setUsername] = useState('philiponions')
   const [authorId, setAuthorId] = useState('1')
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-
+  
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage/>}/>
-        <Route
-          path="/"
-          element={
-            <LoginPage
-              setIsAuthenticated={setIsAuthenticated}
-              isAuthenticated={isAuthenticated}
+    <UserContext.Provider value = {{username, authorId}}>
+      <BrowserRouter>
+        <Routes>            
+            <Route
+              path="/"
+              element={
+                <LoginPage
+                  setIsAuthenticated={setIsAuthenticated}
+                  isAuthenticated={isAuthenticated}
+                />
+              }
             />
-          }
-        />
-        <Route path="/manageposts" element={<ManagePosts/>}/> /* Merge with /posts? */
-        <Route path="/manageposts/restrictions" element={<Restrictions/>}/> /* Merge with /posts? */
-      </Routes>
-    </BrowserRouter>
-
+            <Route
+              path="/homepage"
+              element={
+                isAuthenticated ? <Stream /> : <Navigate to="/" />
+              }
+            />
+            <Route path="/manageposts" element={<ManagePosts/>}/> /* Merge with /posts? */
+            <Route path="/manageposts/restrictions" element={<Restrictions/>}/> /* Merge with /posts? */
+        </Routes>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
