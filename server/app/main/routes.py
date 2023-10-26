9d59ca982d45e06e1d7a99eb0cee164bdcc799c8
Andrew Rosario <71676291+andrew-rosario@users.noreply.github.com>
2023-10-25 18:16:16 -0600

    Move edit_posts route to main/root folder.

--- server/app/main/routes.py
+++ server/app/main/routes.py
@@ -1,6 +1,73 @@
 from app.main import bp
+from app.db import get_db_connection
+from flask import abort, request
+from werkzeug.exceptions import HTTPException
 
 
 @bp.route('/')
 def index():
     return 'This is The Main Blueprint'
+
+
+@bp.route('/service/authors/<author_id>/posts/<post_id>/image', methods=['GET'])
+def get_image(author_id, post_id):
+    # check if image exists
+    try:
+        pass
+    except:
+        pass
+    return None
+
+
+@bp.route('/service/authors/<author_id>/posts/<post_id>', methods=['POST'])
+def edit_post(author_id, post_id):
+    final_message = ""
+    conn = get_db_connection()
+    try:
+        # check if the post exists
+        to_find = (post_id,)
+        print(f"Attempting to find post with {post_id}")
+        cursor = conn.cursor()
+        query = "SELECT * FROM posts WHERE post_id = ?;"
+        conn.execute(query, to_find)
+        exists = cursor.fetchall()
+        if exists is None:
+            abort(404)
+    except HTTPException as e:
+        print(e.code, " Can't find post_id.")
+        final_message = str(e)
+
+    # Get the new data.
+    try:
+        request_data = request.get_json()
+    except Exception as e:
+        print(e, "This is (probably) not a valid JSON file. Or it was sent as key/value pairs.")
+    post_id = to_find[0]
+    title = request_data["title"]
+
+    # At the moment, I am assuming the content provided is of this content_type. Some entry validation might be needed.
+    content_type = request_data["content_type"]
+    content = request_data["content"]
+    image_id = request_data["img_id"]
+    image_id = "NULL" if image_id is None else image_id
+    visibility = request_data["visibility"]
+
+    # The things that will not change:
+    # - Date posted
+    # - Author ID
+    # - Post ID
+
+    # Overwrite the entry with the new data.
+    # TODO: Only update what has changed. Comparison with old vs. new data needed.
+    try:
+        query = "UPDATE posts SET title = ?, content_type = ?,content = ?, img_id = ?, visibility = ? WHERE post_id = ? AND author_id = ?"
+        conn.execute(query, (title, content_type, content, image_id, visibility, post_id, author_id))
+        conn.commit()
+    except Exception as e:
+        print(e, "Data not written to database. Rolling back...")
+        final_message = str(e)
+        conn.rollback()
+    finally:
+        conn.close()
+
+    return final_message
--- server/app/posts/routes.py
+++ server/app/posts/routes.py
@@ -1,8 +1,10 @@
 from app.posts import bp
 import json
 from app.db import get_db_connection
-from flask import request
+from flask import request, abort
+from werkzeug.exceptions import HTTPException
 from random import randrange
+
 
 # VIEW POSTS
 @bp.route('/', methods=['POST'])
@@ -13,9 +15,9 @@ def index():
         print("data")
         request_data = request.get_json()
         author_id = request_data['author_id']
-                
+
         conn = get_db_connection()
-        
+
         # Get all the posts from people who I'm following + posts who are public
         query = "SELECT username, post_id, date_posted, title, content_type, content, img_id, visibility FROM posts " \
                 "INNER JOIN authors ON posts.author_id = authors.author_id " \
@@ -23,18 +25,19 @@ def index():
                 "(SELECT author_following FROM friends WHERE author_followee = ?) " \
                 "OR visibility = 'public' " \
                 "ORDER by date_posted DESC"
-        
-        posts = conn.execute(query, (author_id, )).fetchall()                                
+
+        posts = conn.execute(query, (author_id,)).fetchall()
         conn.commit()
         conn.close()
 
         data = json.dumps([dict(i) for i in posts])
-    
+
     except Exception as e:
         print(e)
         data = str(e)
 
-    return data # data
+    return data  # data
+
 
 # MAKE POSTS
 @bp.route('/new/', methods=['POST'])
@@ -55,84 +58,43 @@ def new_post():
 
         # Determine type of content
         # TODO: only plain text for now, add markdown
-        content_type = "text/plain"
-        
+        content_type = request_data["content_type"]
+
+        # validate the content_type is of the following,
+        try:
+            found = ["text/plain", "text/markdown", "application/base64", "image/png;base64",
+                     "image/jpeg;base64"].index(content_type)
+        except ValueError:
+            print("Not a valid content type.")
+            abort(412)
+
         # Check for image OR image post
         # TODO: add image posting, attaching images to posts
         image_id = request_data["image_id"]
-        
-        if image_id == None: # JSON `null` turns into Python `None`
-            image_id = "NULL" # Change for SQL syntax
-                
+
+        if image_id == None:  # JSON `null` turns into Python `None`
+            image_id = "NULL"  # Change for SQL syntax
+
         conn = get_db_connection()
-        
+
         query = "INSERT INTO posts (post_id, author_id, title, content_type, content, img_id, visibility) " \
-                    "VALUES (?, ?, ?, ?, ?, ?, ?)"
-        
+                "VALUES (?, ?, ?, ?, ?, ?, ?)"
+
         # Use a parameterized query to insert values safely
         conn.execute(query,
-                    (post_id, author_id, title, content_type, content, image_id, visibility))
+                     (post_id, author_id, title, content_type, content, image_id, visibility))
 
         conn.commit()
         conn.close()
-    
+
     except Exception as e:
+
         print(e)
         data = str(e)
 
-    return data # data
-
-@bp.route('/edit/<int:post_id>', methods=['POST'])
-def edit_post():
-    conn = get_db_connection()
-    try:
-        # assert the post_id exists
-        to_find = request.args.get("post_id")
-        print(f"Attempting to find post with {to_find}")
-        query = f'SELECT COUNT(1) FROM posts WHERE post_id = ?'
-        conn.execute(query, (to_find))
-        exists = conn.fetchone()
-        if exists is None:
-            raise Exception("Post Does Not Exist Error")
-    except Exception as e:
-        print(e, " Can't find post_id.")
-        abort()
-
-    # Get the new data.
-    request_data = request.get_json()
-    post_id = to_find
-    title = request_data["title"]
-
-    # At the moment, I am assuming the content provided is of this content_type. Some entry validation might be needed.
-    content_type = request_data["content_type"]
-    content = request_data["content"]
-    image_id = request_data["image_id"]
-    image_id = "NULL" if image_id is None else image_id
-    visibility = request_data["visibility"]
-
-    # The things that will not change:
-    # - Date posted
-    # - Author ID
-    # - Post ID
-
-    # Overwrite the entry with the new data.
-    try:
-        query = ("UPDATE posts"
-                 "SET title = ?, content_type = ?,content, img_id = ?, visibility = ? "
-                 "WHERE post_id = ?")
-        conn.execute(query, (title, content_type, content, image_id, visibility))
-        conn.commit()
-    except Exception as e:
-        #
-        print(e, "Data not written to database. Rolling back...")
-        conn.rollback()
-    finally:
-        conn.close()
+    return data  # data
 
 
-@bp.route('/test/')
-def categories():
-    return "Test route for /posts"
 
 @bp.route('/test/')
 def categories():
