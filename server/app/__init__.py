de4dee02a819b8da21b1b93a5405c220d324ada0
philiponions <hueycoy9@gmail.com>
2023-11-14 22:43:14 -0700

    Work on adding nodes

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -59,7 +59,7 @@ class RequestorView(ModelView):
         db.session.commit()
         # navigate to the index_view of the RequestorView
         return redirect(url_for('.index_view'))
-    
+
 
 
 class Post(db.Model):
@@ -93,10 +93,23 @@ class ImageView(ModelView):
     }
     column_labels = dict(view_button='View Image')
 
+class Node(db.Model):
+    __tablename__ = "nodes"
+    node_id = db.Column(db.Integer, primary_key=True)
+    base_url = db.Column(db.Text)
+    node_name = db.Column(db.Text)    
+
+class NodeView(ModelView):
+    can_delete = True
+    form_columns = ["node_id", "base_url", "node_name"]
+    column_list = ["node_id", "base_url", "node_name"]
+    column_searchable_list = ["node_name"]  
+
 admin.add_view(AuthorView(Author, db.session))
 admin.add_view(RequestorView(Requestor, db.session))
 admin.add_view(PostView(Post, db.session))
 admin.add_view(ImageView(Image, db.session))
+admin.add_view(NodeView(Node, db.session))
 
 
 def create_app():
