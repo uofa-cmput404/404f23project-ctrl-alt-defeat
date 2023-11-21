8b56a72fd1f9d3bafad2a799ea8240f0426553e2
Andrew Rosario <71676291+andrew-rosario@users.noreply.github.com>
2023-11-21 13:16:26 -0700

    Correct schema due to main branch merge

--- server/app/schema.sql
+++ server/app/schema.sql
@@ -3,7 +3,6 @@ DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS authors;
 DROP TABLE IF EXISTS requestors;
 DROP TABLE IF EXISTS admins;
-DROP TABLE IF EXISTS image_post;
 DROP TABLE IF EXISTS friends;
 DROP TABLE IF EXISTS follow_requests;
 DROP TABLE IF EXISTS likes;
@@ -18,9 +17,7 @@ CREATE TABLE posts (
     title TEXT NOT NULL,
     content_type TEXT NOT NULL DEFAULT "text/plain",
     content TEXT NOT NULL,
-    image_id TEXT,
     visibility TEXT NOT NULL DEFAULT "public",
-    FOREIGN KEY (image_id) REFERENCES image_post(img_id) ON DELETE SET NULL,
     FOREIGN KEY (author_id) REFERENCES authors(author_id) ON DELETE CASCADE
 );
 
@@ -41,15 +38,6 @@ CREATE TABLE admins (
     admin_id TEXT PRIMARY KEY,
     username TEXT NOT NULL,
     password TEXT NOT NULL
-);
-
-CREATE TABLE image_post (
-    img_id TEXT PRIMARY KEY,
-    author_id TEXT NOT NULL, --TEXT
-    img_url TEXT NOT NULL,
-    visibility TEXT NOT NULL DEFAULT "public",
-    date_posted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-    FOREIGN KEY (author_id) REFERENCES authors(author_id) ON DELETE CASCADE
 );
 
 CREATE TABLE friends (
