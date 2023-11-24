b71d2fd9032a81a83bed410f27674cbd9050031d
Mark Maligalig <49659278+mark8m@users.noreply.github.com>
2023-11-24 13:24:58 -0700

    add port

--- server/run.py
+++ server/run.py
@@ -2,7 +2,8 @@ from .app import create_app
 
 def app(environ, start_response):
     app = create_app()
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=33507, debug=True)
+    # 33507 = Flask port on Heroku
 
 if __name__ == '__main__':
     app("","")
