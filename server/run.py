58794c4467027f0b896a0f2f131f7ae148a58fdc
Mark Maligalig <49659278+mark8m@users.noreply.github.com>
2023-11-24 13:22:14 -0700

    add host

--- server/run.py
+++ server/run.py
@@ -2,7 +2,7 @@ from .app import create_app
 
 def app(environ, start_response):
     app = create_app()
-    app.run()
+    app.run(host="0.0.0.0", debug=True)
 
 if __name__ == '__main__':
     app("","")
