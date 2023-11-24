434d67a5fcf9f210aed4cd0e1b0970bb3171ae57
Mark Maligalig <49659278+mark8m@users.noreply.github.com>
2023-11-23 20:47:08 -0700

    add run.py

--- server/run.py
+++ server/run.py
@@ -1,8 +1,8 @@
 from .app import create_app
 
-def app():
+def app(environ, start_response):
     app = create_app()
     app.run(debug=True)
 
 if __name__ == '__main__':
-    app()
+    app("","")
