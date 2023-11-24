551ecf41dca422b2f716a79d91cd8d9e80399c79
Mark Maligalig <49659278+mark8m@users.noreply.github.com>
2023-11-23 20:44:44 -0700

    testing run.py

--- server/run.py
+++ server/run.py
@@ -1,6 +1,8 @@
-from app import create_app
+from .app import create_app
 
-if __name__ == '__main__':
+def app():
     app = create_app()
     app.run(debug=True)
-    
+
+if __name__ == '__main__':
+    app()
