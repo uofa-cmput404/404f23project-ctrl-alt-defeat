eb183747fa44658b013fa089d4a745cbc1931a22
Mark Maligalig <49659278+mark8m@users.noreply.github.com>
2023-11-24 13:26:47 -0700

    change run

--- server/run.py
+++ server/run.py
@@ -2,7 +2,7 @@ from .app import create_app
 
 def app(environ, start_response):
     app = create_app()
-    app.run(host="0.0.0.0", port=33507, debug=True)
+    app.run(port=33507, debug=True)
     # 33507 = Flask port on Heroku
 
 if __name__ == '__main__':
