42513d78df2ebdd3343ad27697bf17fa9d8786dd
philiponions <hueycoy9@gmail.com>
2023-11-29 14:44:30 -0700

    Add postlists.js

--- client/social-distribution/src/components/PostsList.js
+++ client/social-distribution/src/components/PostsList.js
@@ -4,10 +4,21 @@ import axios from 'axios';
 
 
 function PostsList(props) {  
+  // Whether user has clicked like
+  const [changeLike, setChangeLike] = useState(false);
+
+  // Post ID of post selected
+  const [postSelected, setPostSelected] = useState(null);
+
+  // Like status of post selected
+  const [postSelectedLiked, setPostSelectedLiked] = useState(null);
+
+  // Author of post selected
+  const [postSelectedAuthor, setPostSelectedAuthor] = useState('');
     
   // Listen for each PostItem if logged in author presses like button
-  const toggleLike = (postSelectedId, postSelectedLiked, postSelectedAuthor) => {
-    console.log("heard postsLists", postSelectedId, postSelectedLiked, postSelectedAuthor);
+  if (changeLike === true) {
+    console.log("heard postsLists", changeLike, postSelected, postSelectedLiked, postSelectedAuthor);
 
     // URL goes to AUTHOR of post, NOT the author who is logged in/sending like
     const sendLikeUrl = "http://127.0.0.1:5000/api/authors/" + postSelectedAuthor + '/inbox';
@@ -19,13 +30,13 @@ function PostsList(props) {
       try {
         axios.post(sendUnlikeUrl, {
           like_author_id: props.authorId,
-          post_id: postSelectedId
+          post_id: postSelected
         })
         .then(response => {
           // Handle successful response
           // Update front-end to reflect change
           props.setPostsLists(props.postsLists.map(item => {
-            if (item.post_id === postSelectedId) {
+            if (item.post_id === postSelected) {
               return { ...item, liked: false}; // Create a new object with the unliked value
             }
             return item; // Return the original object for other items
@@ -47,13 +58,13 @@ function PostsList(props) {
       try {
         axios.post(sendLikeUrl, {
           like_author_id: props.authorId,
-          post_id: postSelectedId
+          post_id: postSelected
         })
         .then(response => {
           // Handle successful response
           // Update front-end to reflect change
           props.setPostsLists(props.postsLists.map(item => {
-            if (item.post_id === postSelectedId) {
+            if (item.post_id === postSelected) {
               return { ...item, liked: true}; // Create a new object with the liked value
             }
             return item; // Return the original object for other items
@@ -69,6 +80,9 @@ function PostsList(props) {
       }
 
     }
+
+
+    setChangeLike(false);
   }
 
   return (
