feb5c4c7915f981d484f7c2a8195d45b9232b452
philiponions <hueycoy9@gmail.com>
2023-12-07 19:50:38 -0700

    Update unit tests for authors

--- server/unittests/mock_schema.sql
+++ server/unittests/mock_schema.sql
@@ -1,38 +1,27 @@
-
-DROP TABLE IF EXISTS posts;
-DROP TABLE IF EXISTS authors;
-DROP TABLE IF EXISTS requestors;
-DROP TABLE IF EXISTS admins;
-DROP TABLE IF EXISTS image_post;
-DROP TABLE IF EXISTS friends;
-DROP TABLE IF EXISTS follow_requests;
-DROP TABLE IF EXISTS likes;
-DROP TABLE IF EXISTS comments;
-DROP TABLE IF EXISTS shares;
+-- Drop tables if they exist
 DROP TABLE IF EXISTS post_restrictions;
-
-CREATE TABLE posts (
-    post_id TEXT PRIMARY KEY,
-    author_id INTEGER NOT NULL,
-    date_posted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    title TEXT NOT NULL,
-    content_type TEXT NOT NULL DEFAULT "text/plain",
-    content TEXT NOT NULL,
-    image_id TEXT,
-    visibility TEXT NOT NULL DEFAULT "public",
-    FOREIGN KEY (image_id) REFERENCES image_post(img_id) ON DELETE SET NULL,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id) ON DELETE CASCADE
-);
-
+DROP TABLE IF EXISTS shares;
+DROP TABLE IF EXISTS comment_likes;
+DROP TABLE IF EXISTS comments CASCADE;
+DROP TABLE IF EXISTS likes;
+DROP TABLE IF EXISTS follow_requests;
+DROP TABLE IF EXISTS friends;
+DROP TABLE IF EXISTS admins;
+DROP TABLE IF EXISTS requestors;
+DROP TABLE IF EXISTS posts CASCADE;
+DROP TABLE IF EXISTS nodes;
+DROP TABLE IF EXISTS authors CASCADE;
+DROP TABLE IF EXISTS inbox_items;
 
 CREATE TABLE authors (
-    author_id INTEGER PRIMARY KEY,
+    author_id TEXT PRIMARY KEY,
     username TEXT NOT NULL,
+    github TEXT,
     password TEXT NOT NULL
 );
 
 CREATE TABLE requestors (
-    requestor_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    requestor_id TEXT PRIMARY KEY,
     username TEXT NOT NULL,
     password TEXT NOT NULL
 );
@@ -43,79 +32,121 @@ CREATE TABLE admins (
     password TEXT NOT NULL
 );
 
-CREATE TABLE image_post (
-    img_id TEXT PRIMARY KEY,
-    author_id INTEGER NOT NULL,
-    img_url TEXT NOT NULL,
-    visibility TEXT NOT NULL DEFAULT "public",
-    date_posted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, 
+-- Create tables
+CREATE TABLE posts (
+    post_id TEXT PRIMARY KEY,
+    author_id TEXT NOT NULL,
+    date_posted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    title TEXT NOT NULL,
+    content_type TEXT NOT NULL DEFAULT 'text/plain',
+    content TEXT NOT NULL,
+    image_id TEXT,
+    visibility TEXT NOT NULL DEFAULT 'PUBLIC',
     FOREIGN KEY (author_id) REFERENCES authors(author_id) ON DELETE CASCADE
 );
 
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE friends (
-    author_followee INTEGER NOT NULL,
-    author_following INTEGER NOT NULL,
-    FOREIGN KEY (author_followee) REFERENCES authors(author_id) ON DELETE CASCADE,
-    FOREIGN KEY (author_following) REFERENCES authors(author_id) ON DELETE CASCADE
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    author_followee TEXT NOT NULL,
+    author_following TEXT NOT NULL,
+    host TEXT NOT NULL
+    --FOREIGN KEY (author_followee) REFERENCES authors(author_id) ON DELETE CASCADE,
+    --FOREIGN KEY (author_following) REFERENCES authors(author_id) ON DELETE CASCADE
 );
 
 CREATE TABLE follow_requests (
-     author_send INTEGER NOT NULL,
-     author_receive INTEGER NOT NULL,
-     FOREIGN KEY (author_send) REFERENCES authors(author_id),
-     FOREIGN KEY (author_receive) REFERENCES authors(author_id)
- );
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    author_send TEXT NOT NULL,
+    author_receive TEXT NOT NULL,    
+    host TEXT DEFAULT 'local' NOT NULL,
+    FOREIGN KEY (author_receive) REFERENCES authors(author_id)
+);
 
 CREATE TABLE likes (
     like_id TEXT PRIMARY KEY,
-    like_author_id INTEGER NOT NULL,
+    like_author_id TEXT NOT NULL,
     post_id TEXT NOT NULL,
-    time_liked TIMESTAMP NOT NULL,
-    FOREIGN KEY (like_author_id) REFERENCES authors(author_id) ON DELETE CASCADE,
-    FOREIGN KEY (post_id) REFERENCES posts(post_id)
+    time_liked TIMESTAMP NOT NULL,    
+    FOREIGN KEY (post_id) REFERENCES posts(post_id) ON DELETE CASCADE
 );
 
 CREATE TABLE comments (
-    post_id TEXT PRIMARY KEY,
-    comment_author_id INTEGER NOT NULL,
-    date_commented TIMESTAMP NOT NULL,
-    FOREIGN KEY (comment_author_id) REFERENCES authors(author_id),
-    FOREIGN KEY (post_id) REFERENCES posts(post_id)
+    comment_id TEXT PRIMARY KEY,
+    comment_author_id TEXT NOT NULL, --TEXT
+    post_id TEXT NOT NULL, --TEXT
+    author_id TEXT NOT NULL, --TEXT
+    comment_text TEXT NOT NULL, --TEXT
+    status TEXT NOT NULL,
+    date_commented TIMESTAMP NOT NULL,    
+    FOREIGN KEY (post_id) REFERENCES posts(post_id)  ON DELETE CASCADE
 );
+
+CREATE TABLE comment_likes (
+    comment_like_id TEXT PRIMARY KEY, --TEXT
+    like_comment_author_id TEXT NOT NULL, --TEXT
+    comment_id TEXT NOT NULL,
+    time_liked TIMESTAMP NOT NULL,
+    FOREIGN KEY (like_comment_author_id) REFERENCES authors(author_id),
+    FOREIGN KEY (comment_id) REFERENCES comments(comment_id) ON DELETE CASCADE
+);
+
 
 CREATE TABLE shares(
     post_id TEXT PRIMARY KEY,
-    share_author_id INTEGER NOT NULL,
+    share_author_id TEXT NOT NULL,
     date_posted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (share_author_id) REFERENCES authors(author_id),
     FOREIGN KEY (post_id) REFERENCES posts(post_id)
 );
 
 CREATE TABLE post_restrictions (
-    post_id INTEGER,
-    restricted_author_id INTEGER,
+    post_id TEXT,
+    restricted_author_id TEXT,
     FOREIGN KEY (post_id) REFERENCES posts (post_id),
-    FOREIGN KEY (restricted_author_id) REFERENCES authors (author_id)
-    UNIQUE (post_id, restricted_author_id) -- Unique constraint
+    FOREIGN KEY (restricted_author_id) REFERENCES authors (author_id),
+    UNIQUE (post_id, restricted_author_id)
 );
 
+CREATE TABLE nodes (
+    node_id INTEGER PRIMARY KEY,
+    node_name TEXT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL   
+);
 
-INSERT INTO posts (post_id, author_id, date_posted, title, content_type, content, image_id, visibility) VALUES
-('post1', 1, '2023-10-29 22:31:02', 'First Post', 'text/plain', 'This is the first post.', 'img1', 'public'),
-('post2', 2, '2023-10-30 08:15:30', 'Second Post', 'text/plain', 'This is the second post.', NULL, 'private'),
-('post3', 1, '2023-10-30 12:45:00', 'Third Post', 'text/markdown', '# This is the third post.', 'img2', 'public');
+-- inbox_item_id = unique identifier within inbox_items
+-- object_id = posts: post_id, follow request: follow_request_id*
+--             post/comment likes: like_id*, comments: comment_id
+--             *these ids only exist in our database
+CREATE TABLE inbox_items (
+    inbox_item_id TEXT NOT NULL PRIMARY KEY,
+    sender_id TEXT NOT NULL,
+    sender_display_name TEXT NOT NULL,
+    sender_host TEXT NOT NULL,
+    recipient_id TEXT NOT NULL,
+    object_id TEXT NOT NULL,
+    type TEXT NOT NULL,
+    date_received TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
 
-INSERT INTO authors (author_id, username, password) VALUES
-(1, 'techgeek5000', 'password1'),
-(2, 'happyuser123', 'password2'),
-(3, 'coolguy456', 'password3'),
-(4, 'jane_doe', 'securepass456');
+INSERT INTO public.authors (
+author_id, username, github, password) VALUES (
+'f2d79922-9571-11ee-b9d1-0242ac120002'::text, 'philiponions'::text, 'philiponions'::text, 'password'::text)
+ returning author_id;
 
-INSERT INTO friends (author_followee, author_following) VALUES
-(2, 3);
+INSERT INTO public.authors (
+author_id, username, password) VALUES (
+'0ca265e4-9572-11ee-b9d1-0242ac120002'::text, 'maven'::text, 'password'::text)
+ returning author_id;
 
-INSERT INTO follow_requests (author_send, author_receive) VALUES
-(2, 1);
+INSERT INTO public.authors (
+author_id, username, github, password) VALUES (
+'2179aff4-9572-11ee-b9d1-0242ac120002'::text, 'mark'::text, 'mark8m'::text, 'password'::text)
+ returning author_id;
 
-INSERT INTO follow_requests (author_send, author_receive) VALUES
-(3, 2);
+ INSERT INTO public.authors (
+author_id, username, password) VALUES (
+'0d3aa4cb-b17d-4090-923f-8ac14185fae6'::text, 'coolguy456'::text, '$2b$12$Z/Tdtse3smqlTWPKv5KHfOt36SVZMPnGexmvOifTtVD5xXU06Ku7.'::text)
+ returning author_id;
